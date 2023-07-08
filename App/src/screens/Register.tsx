f060a968e199bbce0115984d130647b15b8bac3b
Yuri Castilho <yuricastilho2005@gmail.com>
2023-07-08 16:12:42 -0300

    added cafe-img and logo to register page

--- App/src/screens/Register.tsx
+++ App/src/screens/Register.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text} from 'react-native';
+import { View, TouchableOpacity, Text, Image} from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -12,7 +12,19 @@ export function Register() {
     }
   
     return (
-    <View className="flex-1 items-center justify-center">
+    <View className="mx-auto flex-1 bg-[#F5EFEC]">
+        <Image className='mx-auto' 
+        source={require('../../assets/header-register.png')} 
+        />
+    <View className='relative'>
+        <View className='absolute top-1/2 left-1/2'>
+        <View className="mx-auto w-32 h-32 rounded-full bg-[#F5EFEC] border-[#BFA08F] border-4">
+            <Image className='mx-auto mt-2' 
+            source={require('../../assets/logo-cantina.png')}
+            />
+        </View>
+        </View>
+    </View>
         <TouchableOpacity  className='text-center items-center justify-center'
             onPress={openScreen}>
             <Text>
