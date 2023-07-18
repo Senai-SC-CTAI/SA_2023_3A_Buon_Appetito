3e731e6956a73c03564c8ce8ee16e731daa622eb
Yuri Castilho <yuricastilho2005@gmail.com>
2023-07-17 23:24:06 -0300

    pequenas correcao styles Login

--- App/src/screens/Login.tsx
+++ App/src/screens/Login.tsx
@@ -25,13 +25,13 @@ export function Login() {
                 </View>
             </View> */}
             <View className='mx-[50px]'>
-                <View className='items-center justify-center mt-10'>
+                <View className='items-center justify-center mt-24'>
     
                     <Text className='font-medium text-[28px] mt-14'>
                         Login
                     </Text>
     
-                    <View className='gap-7 mt-7'>
+                    <View className='gap-5 mt-3'>
                         <View className='py-3 border-b border-[#D5D5D5] w-[285px]'>
                             <TextInput className='text-[18px]'
                             placeholder='Email'>
@@ -60,7 +60,7 @@ export function Login() {
                 </View>
 
                 <View>
-                    <TouchableOpacity className='mt-5 text-center items-center justify-center px-24 py-2 bg-[#743C34] rounded-[10px]'
+                    <TouchableOpacity className='mt-6 text-center items-center justify-center px-24 py-2 bg-[#743C34] rounded-[10px]'
                         onPress={openScreen}>
                         <Text className='text-white font-medium text-lg'>
                             Entrar
