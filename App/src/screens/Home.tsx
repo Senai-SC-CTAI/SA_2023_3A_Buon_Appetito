93cd5f9fdfde4f1d40b91d90ce3577e92e922fc4
Yuri Castilho <yuricastilho2005@gmail.com>
2023-07-19 23:28:54 -0300

    header quase pronto

--- App/src/screens/Home.tsx
+++ App/src/screens/Home.tsx
@@ -12,13 +12,18 @@ export function Home() {
     }
   
     return (
-    <View className="flex-1 items-center justify-center">
-        <TouchableOpacity className='text-center items-center justify-center'
+    <View className="flex-1">
+        <View className='mt-16 ml-10'>
+            <Text className='text-base text-[#606060]'>Valentina Kroth Gil Cardoso</Text>
+            <Text className='font-medium mt-2 text-4xl text-[#402218]'>Seja bem-vindo!</Text>
+            <Text className='mt-2 text-xl text-[#606060]'>Veja as opções do dia para a sua refeição</Text>
+        </View>
+        {/* <TouchableOpacity className='text-center items-center justify-center'
             onPress={openScreen}>
             <Text>
                 Voltar ao Welcome
             </Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
     </View>
   );
 }
