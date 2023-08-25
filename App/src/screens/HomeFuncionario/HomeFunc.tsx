18b515aa0da760ab86e7c591d5ae63dd91a51062
Yuri Castilho <yuricastilho2005@gmail.com>
2023-08-25 16:25:42 -0300

    navegacao homefunc criada

---
+++ App/src/screens/HomeFuncionario/HomeFunc.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, TextInput} from 'react-native';
+
+import styles from './HomeFuncStyles'
+
+import { useNavigation } from '@react-navigation/native';
+
+
+export function HomeFunc() {
+    const navigation = useNavigation();
+  
+    function openScreen(){
+        navigation.navigate('welcome')
+    }
+
+    return (
+    <View style={styles.main}>
+        <View>
+            <Text style={styles.nome}>Valentina Kroth Gil Cardoso</Text>
+            <Text style={styles.titulo}>Cardápio Ca</Text>
+            <Text style={styles.subtitulo}>Veja as opções do dia para a sua refeição</Text>
+        </View>
+
+        <View style={styles.container}>
+        <TextInput 
+            style={styles.pesquisar}
+                placeholder='Pesquisar...'>
+            </TextInput>
+
+            <TouchableOpacity style={styles.btn}
+                onPress={openScreen}>
+            </TouchableOpacity>
+        </View>
+
+
+        {/* <TouchableOpacity className='text-center items-center justify-center'
+            onPress={openScreen}>
+            <Text>
+                Voltar ao Welcome
+            </Text>
+        </TouchableOpacity> */}
+    </View>
+  );
+}
