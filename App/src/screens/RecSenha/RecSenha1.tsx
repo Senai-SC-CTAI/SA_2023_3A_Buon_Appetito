2d781314a06878a380ec9d425c17f7c1544c8dd8
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-08-25 10:04:12 -0300

    Update RecSenha1.tsx

--- App/src/screens/RecSenha/RecSenha1.tsx
+++ App/src/screens/RecSenha/RecSenha1.tsx
@@ -1,62 +1,39 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, TextInput, Image} from 'react-native';
+import { View, TouchableOpacity, Text, TextInput, Image } from 'react-native';
 
-import styles from './RecSenha1Styles'
+import styles from './RecSenha1Styles';
 
 import { useNavigation } from '@react-navigation/native';
 
 import HeaderImage from '../../Components/HeaderImage';
 
-
-
 export function RecSenha1() {
     const navigation = useNavigation();
-  
-    function openScreen(){
-        navigation.navigate('recsenha2')
+
+    function openScreen() {
+        navigation.navigate('recsenha2');
     }
-  
+
     return (
         <View style={styles.main}>
-    
             <HeaderImage />
-    
-            {/* <View className='relative'>
-                <View className='absolute'>
-                <View className="mx-auto w-32 h-32 rounded-full bg-[#F5EFEC] border-[#BFA08F] border-4">
-                    <Image className='mx-auto mt-2' source={require('../../assets/logo-cantina.png')}/>
-                </View>
-                </View>
-            </View> */}
-            <View style={styles.container}>
-             
-                    
-                        <Text style={styles.titulo}>
-                            Recuperar Senha
-                        </Text>
-                        <Text style={styles.titulo2}>
-                        Digite o email para enviarmos
-                        o código de recuperação
-                        </Text>
-                    <View style={styles.container_info}>
-                        <View style={styles.info_caixa}>
-                            <TextInput style={styles.info_caixa_text}
-                            placeholder='Email'>
-                            </TextInput>
-                        </View>
 
+            <View style={styles.container}>
+                <Text style={styles.titulo}>Recuperar Senha</Text>
+                <Text style={styles.titulo2}>Digite o email para enviarmos o código de recuperação</Text>
+                <View style={styles.container_info}>
+                    <View style={styles.info_caixa}>
+                        <TextInput style={styles.info_caixa_text} placeholder="Email" />
                     </View>
                 </View>
-                <View style={styles.container2}>
-                    <TouchableOpacity style={styles.btn}
-                     onPress={openScreen}>
-                        <Text style={styles.btn_text}>
-                            Próximo
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-                
             </View>
-  
-      );
+
+            <View style={styles.container2}>
+                <View style={{ flex: 1 }} /> {/* Espaço flexível */}
+                <TouchableOpacity style={styles.btn} onPress={openScreen}>
+                    <Text style={styles.btn_text}>Próximo</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
