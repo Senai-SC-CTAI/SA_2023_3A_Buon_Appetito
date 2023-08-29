611aeb4f8a63a0e750abed56bc3716464af43555
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-08-29 12:28:23 -0300

    Update RecSenha2.tsx

--- App/src/screens/RecSenha/RecSenha2.tsx
+++ App/src/screens/RecSenha/RecSenha2.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, TouchableOpacity, Text, TextInput, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Importe o ícone que deseja usar
 
 import styles from './RecSenha2Styles';
 
@@ -8,7 +9,7 @@ import HeaderImage from '../../Components/HeaderImage';
 
 export function RecSenha2() {
     const navigation = useNavigation();
-  
+    const codeInputs = Array(6).fill(0).map(() => useRef(null));
 
     function openScreen() {
         navigation.navigate('recsenha3');
@@ -18,6 +19,19 @@ export function RecSenha2() {
         navigation.navigate('recsenha1');
     }
 
+    function handleInputChange(text, index) {
+        if (text) {
+            if (index < codeInputs.length - 1) {
+                codeInputs[index + 1].current.focus();
+            }
+            
+        } else {
+            if (index > 0) {
+                codeInputs[index - 1].current.focus();
+            }
+        }
+    }
+
     return (
         <View style={styles.main}>
             <HeaderImage />
@@ -25,43 +39,41 @@ export function RecSenha2() {
             <View style={styles.container}>
                 <Text style={styles.titulo}>Recuperar Senha</Text>
                 <Text style={styles.titulo2}>
-                    Digite o código que enviamos
+                    Digite o código que enviamos <br />
                     para o seu email
                 </Text>
 
                 <View style={styles.codeContainer}>
-                    <View style={styles.codeInputContainer}>
-                        <TextInput style={styles.codeInput} maxLength={1} />
-                    </View>
-
-                    <View style={styles.codeInputContainer}>
-                        <TextInput style={styles.codeInput} maxLength={1} />
-                    </View>
-
-                    <View style={styles.codeInputContainer}>
-                        <TextInput style={styles.codeInput} maxLength={1} />
-                    </View>
-
-                    <View style={styles.codeInputContainer}>
-                        <TextInput style={styles.codeInput} maxLength={1} />
-                    </View>
+                    {codeInputs.slice(0, 4).map((ref, index) => (
+                        <View style={styles.codeInputContainer} key={index}>
+                            <TextInput
+                                style={styles.codeInput}
+                                maxLength={1}
+                                onChangeText={(text) => handleInputChange(text, index)}
+                                ref={ref}
+                            />
+                        </View>
+                    ))}
                 </View>
 
                 <View style={styles.centeredCodeContainer}>
-                    <View style={styles.codeInputContainer}>
-                        <TextInput style={styles.codeInput} maxLength={1} />
-                    </View>
-
-                    <View style={styles.codeInputContainer}>
-                        <TextInput style={styles.codeInput} maxLength={1} />
-                    </View>
+                    {codeInputs.slice(4).map((ref, index) => (
+                        <View style={styles.codeInputContainer} key={index + 4}>
+                            <TextInput
+                                style={styles.codeInput}
+                                maxLength={1}
+                                onChangeText={(text) => handleInputChange(text, index + 4)}
+                                ref={ref}
+                            />
+                        </View>
+                    )).reverse()}
                 </View>
             </View>
 
             <View style={styles.container2}>
                 <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.btn} onPress={openScreenrecsenha1}>
-                        <Text style={styles.btn_text}>Voltar</Text>
+                    <TouchableOpacity style={styles.btn2} onPress={openScreenrecsenha1}>
+                        <Text style={styles.btn_text2}>Voltar</Text>
                     </TouchableOpacity>
 
                     <View style={styles.buttonSeparator}></View>
