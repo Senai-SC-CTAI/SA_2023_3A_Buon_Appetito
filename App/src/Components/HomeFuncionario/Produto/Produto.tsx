61a9f6e3de0ce81fe447cc038e4c2eee2809f445
Yuri Castilho <yuricastilho2005@gmail.com>
2023-08-29 21:31:49 -0300

    Model EditProduto adicionado

--- App/src/Components/HomeFuncionario/Produto/Produto.tsx
+++ App/src/Components/HomeFuncionario/Produto/Produto.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Modal } from 'react-native';
 import styles from './ProdutoStyles'
 
+
 export function Produto() {
+      const [modalVisible, setModalVisible] = useState(false);
     return (
-        <TouchableOpacity>
-            <View style={styles.container}>
-                <Text style={styles.nome}>Imagem</Text>
-                <Text style={styles.nome}>Produto</Text>
-                <TouchableOpacity>
-                    <Text style={styles.nome}>Excluir</Text>
-                </TouchableOpacity>
-            </View>
-        </TouchableOpacity>
+        <View>
+
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+            setModalVisible(!modalVisible);
+            }}>
+
+
+                
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Hello World!</Text>
+
+
+
+                        <TouchableOpacity
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => setModalVisible(!modalVisible)}>
+                            <Text style={styles.textStyle}>Editar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+
+            <TouchableOpacity>
+                <View style={styles.container}>
+                    <Text style={styles.nome}>Imagem</Text>
+                    <Text style={styles.nome}>Produto</Text>
+                    <TouchableOpacity
+                    onPress={() => setModalVisible(true)}>
+                        <Text style={styles.nome}>Excluir</Text>
+                    </TouchableOpacity>
+                </View>
+            </TouchableOpacity>
+        </View>
   );
 }
