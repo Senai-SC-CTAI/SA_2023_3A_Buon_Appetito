841a99d3840458c0231a53ca2b5096953c9d0af8
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-11-13 16:16:43 -0300

    Update HeaderStyles.tsx

--- App/src/Components/Home/Header/HeaderStyles.tsx
+++ App/src/Components/Home/Header/HeaderStyles.tsx
@@ -1,61 +1,66 @@
 import { StyleSheet } from "react-native";
 
 const HeaderStyles = StyleSheet.create({
-    container:{
-        flex: 1/1.5,
+    container: {
+        flex: 1 / 1.5,
         width: '100%',
         height: '50%',
         alignItems: 'center',
     },
-    text:{
+    text: {
         width: 300,
-        marginTop: 40
-
+        marginTop: 40,
     },
-    nome:{
-        fontSize: 16, 
+    nome: {
+        fontSize: 16,
         fontWeight: '300',
         textAlign: 'left',
-        color: '#606060'
+        color: '#606060',
     },
-    titulo:{
-        fontSize: 40, 
+    titulo: {
+        fontSize: 40,
         textAlign: 'left',
         color: '#402218',
-        marginTop: 6
+        marginTop: 6,
     },
-    descricao:{
+    descricao: {
         fontSize: 22,
         textAlign: 'left',
         color: '#606060',
         marginRight: 50,
-        marginTop: 6
+        marginTop: 6,
     },
-    buttons:{
+    buttons: {
         flexDirection: 'row',
-        marginTop: 60
+        marginTop: 20, // Ajuste a distância vertical entre os botões e a barra superior
     },
-    search:{
+    search: {
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: 'rgb(245, 245, 245)',
         borderRadius: 100,
         padding: 16,
-        paddingHorizontal: 10
+        paddingHorizontal: 10,
+        marginTop: 10, // Ajuste a distância vertical do botão de pesquisa
     },
-    search_input:{
+    search_input: {
         fontSize: 16,
-        paddingLeft: 6
+        paddingLeft: 6,
     },
-    settings:{
+    settings: {
         marginLeft: 30,
         alignItems: 'center',
         justifyContent: 'center',
         width: 50,
         height: 50,
         borderRadius: 999,
-        backgroundColor: '#743C34'
+        backgroundColor: '#743C34',
+        marginTop: 10, // Ajuste a distância vertical do botão de configurações
     },
-})
+  
+}
+)
 
-export default HeaderStyles
+export default HeaderStyles;
+
+
