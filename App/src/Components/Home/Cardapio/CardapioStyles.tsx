9143408ae4b80f54cffac4d2e9169951f3183e9e
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-11-15 15:07:56 -0300

    Update CardapioStyles.tsx

--- App/src/Components/Home/Cardapio/CardapioStyles.tsx
+++ App/src/Components/Home/Cardapio/CardapioStyles.tsx
@@ -1,20 +1,20 @@
 import { StyleSheet } from "react-native";
 
 const CardapioStyles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#DDC6BA',
-        width: '100%',
-        borderTopLeftRadius: 80,
-        borderTopRightRadius: 80
-    },
-    posicionamento:{
-        marginTop: 24,
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-        
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#DDC6BA',
+    width: '100%',
+    borderTopLeftRadius: 80,
+    borderTopRightRadius: 80,
+  
+  },
+  posicionamento: {
+    marginTop: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+});
 
-export default CardapioStyles
+export default CardapioStyles;
