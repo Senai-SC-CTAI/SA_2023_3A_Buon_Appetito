87c6cf73f9bf87f04f2300bae3c09e92605df590
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-11-15 15:08:44 -0300

    Update ProdutoStyles.tsx

--- App/src/Components/Home/Produto/ProdutoStyles.tsx
+++ App/src/Components/Home/Produto/ProdutoStyles.tsx
@@ -1,20 +1,49 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const ProdutoStyles = StyleSheet.create({
-    container:{
-        marginTop: 50,
-        borderRadius: 10,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-        backgroundColor: '#FCFCFC',
-        width: 300
-    },
-    nome:{
-        fontSize: 22
-    }
-})
+  container: {
+    marginTop: 20,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+    width: 300,
+    overflow: 'hidden',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    backgroundColor: '#FCFCFC',
+  },
+  imagem: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  nome: {
+    fontSize: 22,
+    color: '#151515',
 
-export default ProdutoStyles
+  },
+  cardContent: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  subtitulo: {
+    fontSize: 18,
+    color: '#A07964',
+    
+
+  },
+  descricao: {
+    fontSize: 16,
+    color: '#515151',
+   
+    fontWeight: '300',
+    marginTop: 1.5,
+    marginBottom: 6,
+  },
+});
+
+export default ProdutoStyles;
