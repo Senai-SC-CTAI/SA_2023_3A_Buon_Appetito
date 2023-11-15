e9c61d9743d57e5906541100d7086b04d886a4fd
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-11-15 15:08:24 -0300

    Update Produto.tsx

--- App/src/Components/Home/Produto/Produto.tsx
+++ App/src/Components/Home/Produto/Produto.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-import { View, Image, Text, TouchableOpacity } from 'react-native';
-import styles from './ProdutoStyles'
+import ProdutoStyles from './ProdutoStyles';
 
-export function Produto() {
-    return (
-        <TouchableOpacity>
-            <View style={styles.container}>
-                <Text style={styles.nome}>Imagem</Text>
-                <Text style={styles.nome}>Produto</Text>
-            </View>
-        </TouchableOpacity>
+export function Produto({ nome, imagem, ingredientes, preco }) {
+  const [isCardOpen, setIsCardOpen] = useState(false);
+
+  const handleToggleCard = () => {
+    setIsCardOpen(!isCardOpen);
+  };
+
+  return (
+    <TouchableOpacity onPress={handleToggleCard}>
+      <View style={ProdutoStyles.container}>
+        <View style={ProdutoStyles.header}>
+          <Image source={{ uri: imagem }} style={ProdutoStyles.imagem} />
+          <Text style={ProdutoStyles.nome}>{nome}</Text>
+          <AntDesign name={isCardOpen ? 'up' : 'down'} size={22} color="#151515" />
+        </View>
+        {isCardOpen && (
+          <View style={ProdutoStyles.cardContent}>
+            <Text style={ProdutoStyles.subtitulo}>Ingredientes do produto</Text>
+            <Text style={ProdutoStyles.descricao}>{ingredientes}</Text>
+            <Text style={ProdutoStyles.subtitulo}>Preço</Text>
+            <Text style={ProdutoStyles.descricao}>{preco}</Text>
+          </View>
+        )}
+      </View>
+    </TouchableOpacity>
   );
 }
